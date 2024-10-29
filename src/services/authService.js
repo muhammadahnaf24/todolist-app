9106f74c8423b422bc82542e4b398d96f77f0edc
muhammadahnaf24 <muhammadahnav@gmail.com>
2024-10-29 21:06:15 +0700

    update

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,8 @@
 // src/services/authService.js
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/auth"; // Sesuaikan URL backend Anda
-
-// Register
+const API_URL =
+  "https://vercel.com/api/toolbar/link/todolist-app-server.vercel.app?via=project-dashboard-alias-list&p=1&page=/auth";
 export const register = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
   return response.data;
